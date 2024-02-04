83e1b8b6912226f12184a835d7b8ef87aded0d76
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2024-02-03 18:03:11 -0800

    Add network architecture

--- processing_mnist/train.py
+++ processing_mnist/train.py
@@ -1,7 +1,19 @@
 import numpy
 import torch
 import torchvision
+import torch.nn.functional as F
 import sys
+
+NUM_EPOCHS = 3
+BATCH_SIZE = 64
+TEST_BATCH_SIZE = 1000
+LEARNING_RATE = 0.01
+MOMENTUM = 0.5
+LOG_INTERVAL = 10
+
+torch.backends.cudnn.enabled = False
+torch.manual_seed(1)
+
 train_loader = torch.utils.data.DataLoader(
     torchvision.datasets.MNIST('datasets/', train=True, download=True,
                                transform=torchvision.transforms.Compose([
@@ -19,3 +31,26 @@ test_loader = torch.utils.data.DataLoader(
                                        (0.1307,), (0.3081,))
                                ])),
     batch_size=TEST_BATCH_SIZE, shuffle=True)
+
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = torch.nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = torch.nn.Dropout2d()
+        self.fc1 = torch.nn.Linear(320, 50)
+        self.fc2 = torch.nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+
+nn = Net()
+optimizer = torch.optim.SGD(nn.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)

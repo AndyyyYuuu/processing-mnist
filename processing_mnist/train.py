80c6df6d0355f7e9a7d4e7d8ed4c64ffb2405135
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2024-02-04 11:03:22 -0800

    Add training

--- processing_mnist/train.py
+++ processing_mnist/train.py
@@ -2,7 +2,9 @@ import numpy
 import torch
 import torchvision
 import torch.nn.functional as F
+from tqdm import tqdm
 import sys
+from model import Net
 
 NUM_EPOCHS = 3
 BATCH_SIZE = 64
@@ -32,25 +34,39 @@ test_loader = torch.utils.data.DataLoader(
                                ])),
     batch_size=TEST_BATCH_SIZE, shuffle=True)
 
-
-class Net(torch.nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = torch.nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = torch.nn.Dropout2d()
-        self.fc1 = torch.nn.Linear(320, 50)
-        self.fc2 = torch.nn.Linear(50, 10)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x)
-
-
 nn = Net()
 optimizer = torch.optim.SGD(nn.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
+
+def train(epoch):
+    nn.train()
+    for batch_idx, (data, target) in enumerate(tqdm(train_loader)):
+        optimizer.zero_grad()
+        output = nn(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % LOG_INTERVAL == 0:
+            torch.save(nn.state_dict(), 'models/model_1.pth')
+            torch.save(optimizer.state_dict(), 'models/optimizer_1.pth')
+    print(f"--Epoch {epoch}-- \n  Loss: {loss.item()}")
+
+
+def test():
+    nn.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = nn(data)
+            test_loss += F.nll_loss(output, target, size_average=False).item()
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).sum()
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+for epoch in range(1, NUM_EPOCHS + 1):
+    train(epoch)
+    test()
